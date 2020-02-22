7576ca831a9e566fca344898d36fd05db535647e
Laurynas <laurynas@noia.network>
2020-02-22 18:27:37 +0200

    added pagination logic in backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,10 +6,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
 try {
   app.use("/products", function(req, res) {
+    const page = req.query.page;
+    const limit = req.query.limit;
+      
+    const skip = (page - 1) * limit;
+ 
     con.query(
-      'SELECT filename, product_name, actual_price, brand_name FROM products_list LIMIT 52',
+      `SELECT filename, product_name, actual_price, brand_name FROM products_list LIMIT ${limit} OFFSET ${skip}`,
       async (err, data) => {
         if (err) throw err;
         await res.json(data)
