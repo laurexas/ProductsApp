ed443007322160c8dbacec1017661d45215c37ce
Laurynas <laurynas@noia.network>
2020-02-23 21:38:36 +0200

    pagination logic

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,16 +9,21 @@ app.use(express.json());
 
 try {
   app.use("/products", function(req, res) {
+    let count;
     const page = req.query.page;
     const limit = req.query.limit;
-      
     const skip = (page - 1) * limit;
+    const query = `SELECT COUNT(*) as total_products FROM products_list`;
+    con.query(query, (err,data) => {
+      if(err) throw err;
+      count = data[0].total_products
+    })
  
     con.query(
       `SELECT filename, product_name, actual_price, brand_name FROM products_list LIMIT ${limit} OFFSET ${skip}`,
       async (err, data) => {
         if (err) throw err;
-        await res.json(data)
+        await res.json({"total" : count, "data": data})
       }
     );
   });
